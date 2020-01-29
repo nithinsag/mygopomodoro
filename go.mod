1614db6bddb83024c78a22b919d4ce577a481467
Nithin Saji <nithin@fabelio.com>
2020-01-30 00:57:37 +0700

    make sound after x minutes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/diadara/mypomodoro
 
 go 1.13
 
-require github.com/faiface/beep v1.0.2
+require (
+	github.com/faiface/beep v1.0.2
+	golang.org/x/tools v0.0.0-20200129045341-207d3de1faaf // indirect
+)
--- mypomodoro.go
+++ mypomodoro.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/faiface/beep"
@@ -10,8 +12,8 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-func main() {
-	f, err := os.Open("ketchup.mp3")
+func playFileAndBlock(file string) {
+	f, err := os.Open(file + ".mp3")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,12 +25,29 @@ func main() {
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	done := make(chan bool)
-// Callback creates a streamer which calls our callback
-// Seq stiches the streamers together
-// And Play plays the 
+	// Callback creates a streamer which calls our callback
+	// Seq stiches the streamers together
+	// And Play plays the
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		done <- true
 	})))
 
 	<-done
+}
+
+func main() {
+	var countdown int64
+	countdown = 30
+	if len(os.Args) > 1 {
+		var err error
+		countdown, err = strconv.ParseInt(os.Args[1], 10, 64)
+		if err != nil {
+			log.Fatal("optional argument is the number of minutes for your pomodoro: eg. 20")
+		}
+
+	}
+	fmt.Printf("Starting countdown with %d minute\n", countdown)
+	// donetimer := make(chan bool)
+	time.Sleep(time.Duration(countdown) * time.Minute)
+	playFileAndBlock("ketchup")
 }
